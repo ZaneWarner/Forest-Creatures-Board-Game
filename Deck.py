913a7e605c47ce659679e676b202173bd76f3994
ZaneWarner <zane.warner@gmail.com>
2019-05-22 14:37:13 -0700

    minor bugfixes

--- Deck.py
+++ Deck.py
@@ -5,16 +5,16 @@ class Deck:
     #each zone has an 8 element array, with elements corresponding to the number of squirrels, beavers, owls, 
     #   their upgraded forms, wild cards, and total card count, respectively
     def __init__(self):
-        self.deck = [5]*3 + [0]*4 + [15]
+        self.deck = [6]*3 + [0]*4 + [15]
         self.hand = [0]*8
         self.discard = [0]*8
         
-    def drawFive(self):
-        #draws five random cards from deck to hand. Assumes deck contains at least 5 cards (a truth the game rules maintain)
+    def drawSix(self):
+        #draws six random cards from deck to hand. Assumes deck contains at least 5 cards (a truth the game rules maintain)
         deckExpanded = []
         for i in range(7):
             deckExpanded += [i]*self.deck[i]
-        drawn = np.random.choice(deckExpanded, 5, replace=False)
+        drawn = np.random.choice(deckExpanded, 6, replace=False)
         for card in drawn:
             self.deck[card] -= 1
             self.deck[7] -= 1 #recall that idx 7 tracks the total card count in that zone
@@ -36,6 +36,6 @@ class Deck:
         self.discard = [0]*8
         
     def exchange(self, remove, add):
-        #Removes a card with idx remove, adds one with idx add in its place
-        self.deck[remove] -= 1
-        self.deck[add] += 1
+        #Removes a card with idx remove from the player's hand, adds one with idx add in its place
+        self.hand[remove] -= 1
+        self.hand[add] += 1
